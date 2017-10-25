e032ead11cae283adc0b11b98e28e69a1e142fe6
Irmela <irmela@studentensteuererklaerung.de>
2017-10-25 09:20:22 +0200

    validate vehicle uuid for presence

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,5 @@
 class Vehicle < ApplicationRecord
   has_many :locations, dependent: :destroy
 
-  validates :uuid, uniqueness: true
+  validates :uuid, uniqueness: true, presence: true
 end
--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
   it { should validate_uniqueness_of(:uuid) }
+  it { should validate_presence_of(:uuid) }
 end
