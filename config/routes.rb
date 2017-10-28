c291e6847aeaf7888c7027c26b3909e22d22dd79
Irmela <irmela@berlin-coding.de>
2017-10-28 22:52:26 +0200

    add map controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :vehicles, only: [:create, :destroy] do
+  root 'map#index'
+
+  resources :vehicles, only: [:index, :create, :destroy] do
     resources :locations, only: :create
   end
+
+  get 'map/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
