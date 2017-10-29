bc53e6a3942e97830a2f134e43c36ecfdba38883
Irmela <irmela@berlin-coding.de>
2017-10-29 23:40:33 +0100

    re-oderder methods

--- app/assets/javascripts/components/vehicle_map.jsx
+++ app/assets/javascripts/components/vehicle_map.jsx
@@ -9,19 +9,64 @@ class VehicleMap extends React.Component {
     };
   }
 
-  generateColor(uuid) {
-    return '#' + uuid.substring(1,7);
+  componentDidMount(){
+    this.buildMap();
+
+    this.timer = setInterval(()=> this.getVehicles(), 3000)
   }
 
-  buildPinMarker(color) {
-    return `<span class="Marker Marker--pin" style="background-color: ${color}"></span>`
+  componentWillUpdate() {
+    // ToDo: only update when vehicle state changed
+    this.vehicleLayers.clearLayers();
+    this.addVehicles(this.map);
   }
 
-  buildPopup(vehicle) {
-    latestLocation = vehicle.locations.slice(-1).pop();
-    return `<b>Vehicle:</b> ${vehicle.uuid}<br />
-            <b>Time:</b> ${latestLocation.at}<br />
-            <b>Position:</b> ${latestLocation.lat}, ${latestLocation.lng}`
+  getVehicles() {
+    axios.get('/vehicles.json')
+      .then(res => {
+        const vehicles = res.data;
+        this.setState({ vehicles });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  registerVehicle(vehicle) {
+    axios.post('/vehicles', { id: vehicle.uuid })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  deregisterVehicle(vehicle) {
+    axios.delete(`/vehicles/${vehicle.uuid}`)
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  buildMap() {
+    var map = this.map = L.map('map').setView(d2dOffice, 13);
+
+    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    let homeIcon = L.divIcon({className: 'Marker Marker--home', iconSize: [40, 40]});
+    L.marker(d2dOffice, {icon: homeIcon}).addTo(map);
+
+    this.boundary = L.circle(d2dOffice, {radius: radius, color: '#003552'}).addTo(map);
+
+    this.vehicleLayers = L.markerClusterGroup().addTo(map);
+  }
+
+  addVehicles() {
+    for (var index = 0; index < this.state.vehicles.length; index++) {
+      let vehicle = this.state.vehicles[index];
+
+      this.addVehiclePath(this.map, vehicle, this.boundary)
+    }
   }
 
   addVehiclePath(map, vehicle, boundary) {
@@ -47,65 +92,19 @@ class VehicleMap extends React.Component {
     }
   }
 
-  addVehicles() {
-    for (var index = 0; index < this.state.vehicles.length; index++) {
-      let vehicle = this.state.vehicles[index];
-
-      this.addVehiclePath(this.map, vehicle, this.boundary)
-    }
+  generateColor(uuid) {
+    return '#' + uuid.substring(1,7);
   }
 
-  buildMap() {
-    var map = this.map = L.map('map').setView(d2dOffice, 13);
-
-    L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    let homeIcon = L.divIcon({className: 'Marker Marker--home', iconSize: [40, 40]});
-    L.marker(d2dOffice, {icon: homeIcon}).addTo(map);
-
-    this.boundary = L.circle(d2dOffice, {radius: radius, color: '#003552'}).addTo(map);
-
-    this.vehicleLayers = L.markerClusterGroup().addTo(map);
+  buildPinMarker(color) {
+    return `<span class="Marker Marker--pin" style="background-color: ${color}"></span>`
   }
 
-  getVehicles() {
-    axios.get('/vehicles.json')
-      .then(res => {
-        const vehicles = res.data;
-        this.setState({ vehicles });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-
-  registerVehicle(vehicle) {
-    axios.post('/vehicles', { id: vehicle.uuid })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  deregisterVehicle(vehicle) {
-    axios.delete(`/vehicles/${vehicle.uuid}`)
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  componentDidMount(){
-    this.buildMap();
-
-    this.timer = setInterval(()=> this.getVehicles(), 3000)
-  }
-
-  componentWillUpdate() {
-    // ToDo: only update when vehicle state changed
-    this.vehicleLayers.clearLayers();
-    this.addVehicles(this.map);
+  buildPopup(vehicle) {
+    latestLocation = vehicle.locations.slice(-1).pop();
+    return `<b>Vehicle:</b> ${vehicle.uuid}<br />
+            <b>Time:</b> ${latestLocation.at}<br />
+            <b>Position:</b> ${latestLocation.lat}, ${latestLocation.lng}`
   }
 
   render() {
