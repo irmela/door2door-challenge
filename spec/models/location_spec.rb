e0074ff5af4872bd6e963cc5d18cc68b9e01a4d6
Irmela <irmela@berlin-coding.de>
2017-10-30 08:44:32 +0100

    use is_expected.to over should

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  it { should validate_presence_of(:lat) }
-  it { should validate_presence_of(:lng) }
-  it { should validate_presence_of(:at) }
-  it { should validate_presence_of(:vehicle) }
-  it { should validate_uniqueness_of(:at).scoped_to(:vehicle_id) }
+  it { is_expected.to validate_presence_of(:lat) }
+  it { is_expected.to validate_presence_of(:lng) }
+  it { is_expected.to validate_presence_of(:at) }
+  it { is_expected.to validate_presence_of(:vehicle) }
+  it { is_expected.to validate_uniqueness_of(:at).scoped_to(:vehicle_id) }
+  it { is_expected.to belong_to(:vehicle) }
 end
--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
-  it { should validate_uniqueness_of(:uuid) }
-  it { should validate_presence_of(:uuid) }
+  it { is_expected.to validate_uniqueness_of(:uuid) }
+  it { is_expected.to validate_presence_of(:uuid) }
+  it { is_expected.to have_many(:locations) }
 end
